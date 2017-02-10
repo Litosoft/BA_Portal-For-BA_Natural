26abc12aa80b9367c0349cebf391a62e5913450a
Tyler L <tylersloeper@gmail.com>
2017-02-09 22:51:53 -0500

    Login limited by authorizationg and beautified

--- BA Portal/App_Start/FilterConfig.cs
+++ BA Portal/App_Start/FilterConfig.cs
@@ -8,6 +8,8 @@ namespace BA_Portal
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new System.Web.Mvc.AuthorizeAttribute());
+            //filters.Add(new RequireHttpsAttribute());
         }
     }
 }
