4ab82e587a2268accc878ed8cb4b89e007c343ad
Tyler L <tylersloeper@gmail.com>
2018-03-03 14:27:43 -0500

    DAL layer initial implementation.

--- BA Portal/BANaturalDAL/BaNaturalDAL.cs
+++ BA Portal/BANaturalDAL/BaNaturalDAL.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using BA_Portal.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// DAL, for allowing multiple queries in the same asp mvc page from different tables.
+/// </summary>
+
 namespace BA_Portal.BANaturalDAL
 {
     public class BaNaturalDAL
     {
+        public enum DBcontext_Options
+        {
+            SOAPFormDbContext,
+            SignatureDbContext,
+            SubjectDbContext,
+            PDFDbContext,
+            InsuranceVerificationDbContext,
+            InsuranceInfoDbContext,
+            QuickSoapNoteDbContext
+        }
+
+        // The general db context type for all functions.
+        private System.Data.Entity.DbContext DbInUse;
+
+        // The select DB context function
+        public void SelectDBcontext(DBcontext_Options SelectedDB)
+        {
+            if(SelectedDB == DBcontext_Options.SOAPFormDbContext)
+            {
+                 DbInUse = new SOAPFormDbContext();
+            }
+            else if (SelectedDB == DBcontext_Options.SignatureDbContext)
+            {
+                 DbInUse = new SignatureDbContext();
+            }
+            else if (SelectedDB == DBcontext_Options.SubjectDbContext)
+            {
+                 DbInUse = new SubjectDbContext();
+            }
+            else if (SelectedDB == DBcontext_Options.PDFDbContext)
+            {
+                 DbInUse = new PDFDbContext();
+            }
+            else if (SelectedDB == DBcontext_Options.InsuranceVerificationDbContext)
+            {
+                 DbInUse = new InsuranceVerificationDbContext();
+            }
+            else if(SelectedDB == DBcontext_Options.InsuranceInfoDbContext)
+            {
+                 DbInUse = new InsuranceInfoDbContext();
+            }
+            else if(SelectedDB == DBcontext_Options.QuickSoapNoteDbContext)
+            {
+                 DbInUse = new QuickSoapNoteDbContext();
+            }
+        }
+
+        /// All general database operations.
+        // Select All
+
+        // Select by parameter
+
+        // Delete All
+
+        // Delete by parameter
+
+        // Insert new
     }
 }
---
+++ BA Portal/Models/QuickSoapNote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Globalization;
+
+namespace BA_Portal.Models
+{
+    public class QuickSoapNote
+    {
+        // Quick Note 
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int UniqueID { get; set; }
+        public string ReturnDateRecommended { get; set; }
+        public string HerbalSupplement { get; set; }
+        public string OtherDetails { get; set; }
+
+        // Quick Soap Note
+    }
+
+    public class QuickSoapNoteDbContext : System.Data.Entity.DbContext
+    {
+        public QuickSoapNoteDbContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<QuickSoapNote> QuickSoapNoteDatabase { get; set; }
+    }
+}
